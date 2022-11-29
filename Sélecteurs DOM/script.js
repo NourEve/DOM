e3948f1dfe94b263f970a3e96744387ad4a746c5
Nour Everaert <nour.everaert.prof@gmail.com>
2022-11-29 12:13:44 +0100

    sélecteurs - 2

--- Sélecteurs DOM/script.js
+++ Sélecteurs DOM/script.js
@@ -1,7 +1,14 @@
-/*Ajoutez un titleattribut à chaque élément qui a la classe importante, en indiquant This is an important item. Astuce : ajouter un attribut title à un élément est différent de changer le titre d'un document.*/
+
 const imp = document.querySelectorAll('.important');
 imp.forEach((important) => {
     important.setAttribute("title", "This is an important item");
 })
-/*Sélectionnez toutes les balises img et parcourez-les. S'ils n'ont pas importantde classe, transformez leur propriété d'affichage en none*/
+
+const modif_img = document.querySelectorAll('img');
+for (let elem of modif_img) {
+    if (!elem.classList.contains("important")){
+        elem.style.display ="none";
+    }
+}
+
 
