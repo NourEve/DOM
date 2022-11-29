5301a133f7633639cb8d8b04fba68057b0e7c1f2
Nour Everaert <nour.everaert.prof@gmail.com>
2022-11-29 14:58:45 +0100

    sélecteurs - 3

--- Sélecteurs DOM/script.js
+++ Sélecteurs DOM/script.js
@@ -1,14 +1,29 @@
-
+//partie 1 - title
 const imp = document.querySelectorAll('.important');
 imp.forEach((important) => {
     important.setAttribute("title", "This is an important item");
 })
 
+//partie 2 - display_none
 const modif_img = document.querySelectorAll('img');
 for (let elem of modif_img) {
     if (!elem.classList.contains("important")){
         elem.style.display ="none";
     }
 }
+
+//partie 3 - paragraphe+class
+const parag = document.querySelectorAll('p');
+for (let elem of parag){
+    if (elem.classList.contains(elem.className)){
+        console.log(elem.className);
+        console.log(elem.innerText);
+    }
+    else {
+        console.log(elem.innerText);    
+    }
+}
+
+
 
 
