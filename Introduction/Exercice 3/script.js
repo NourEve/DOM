d5835f3e118e87fdb2568f26e4d9b8560c1f4542
Nour Everaert <nour.everaert.prof@gmail.com>
2022-11-29 10:10:58 +0100

    introduction-3-pas fini

---
+++ Introduction/Exercice 3/script.js
@@ -0,0 +1,15 @@
+/*
+*/
+console.log(document.title);
+document.title = "Modifying the DOM";
+
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 255);
+    var y = Math.floor(Math.random() * 255);
+    var z = Math.floor(Math.random() * 255);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+console.log(bgColor);
+document.body.style.background = bgColor;
+}
+random_bg_color();
