16fdb8b4102372a01ac8e6961116163f518e2e10
Nour Everaert <nour.everaert.prof@gmail.com>
2022-11-30 14:05:45 +0100

    DOM terminé

--- Create/script.js
+++ Create/script.js
@@ -2,14 +2,18 @@
 //tableau des apprenants :
 let inputAr = ["Arno Volts" ,"Aurélien Mariaule" ,"Aurore Lemaire" ,"Benjamin Porsont" ,"Céline Verreydt" ,"Corentin Miserque" ,"Dominique Coppée" ,"Edouard de Romrée de Vichenet" ,"Hugo Goorickx ","Jofrey Houyoux" ,"Jonathan Manes" ,"Jonathan bajoux" ,"Laura Wilhelmi" ,"Lysie Soyez" ,"Marnie Benalia" ,"Mathilde Cornelis" ,"Milo Bonnet" ,"Nadim El Nakadi" ,"Nathalie Vanden Abeele" ,"Nathalie Goffette" ,"Nour Everaert" ,"Pierre Mauriello" ,"Quentin Bource ","Virginie Dourson"];
 
-//fonction sortant une couleur aléatoire :
-function random_bg_color() {
-    var x = Math.floor(Math.random() * 255);
-    var y = Math.floor(Math.random() * 255);
-    var z = Math.floor(Math.random() * 255);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    return bgColor;
+//fonction sortant une couleur aléatoire pour le fond et générant un texte en fonction de la luminosité de ce dernier :
+function random_bg_color(txtModif) {
+    let x = Math.floor(Math.random() * 255);
+    let y = Math.floor(Math.random() * 255);
+    let z = Math.floor(Math.random() * 255);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    const brightness = Math.round(((parseInt(x) * 299) + (parseInt(y) * 587) + (parseInt(z) * 114)) / 1000);
+    let txtColor = (brightness > 125) ? 'black' : 'white';
+        txtModif.style.color = txtColor;
+        txtModif.style.backgroundColor = bgColor;
 }
+
 //fonction sortant les apprenants de manière aléatoire sous forme de tableau :
 function pickLearner(array,n) {
     for (let i=0; i < n; i++){
@@ -18,12 +22,14 @@ function pickLearner(array,n) {
     }
 }
 
-//function prenant un élément du tableau pour le transformer en texte :
-function addElement(inputAr){
-newTab = pickLearner(inputAr,24)
+//fonction prenant un élément du tableau pour le transformer en paragraphe avec ses couleurs aléatoires :
+function addElement(newTab){
 for (let elem of newTab) {
-    let newTxt = document.createTextNode(" "+elem+" ");
-    return newTxt;
+    let txtLearner = document.createTextNode(elem);
+    let newParag = document.createElement('p');
+    newParag.appendChild(txtLearner);
+    random_bg_color(newParag);
+    newSection.appendChild(newParag);
 }
 }
 
@@ -32,15 +38,9 @@ let tagArticle = document.getElementsByTagName("article")[0];
 let newSection = document.createElement('section');
 tagArticle.appendChild(newSection);
 
-//boucle for pour placer la fonction "addElement" dans des paragraphes et placer la couleur de fond :
-let randStart = inputAr.length;
-for (let i=0 ; i<randStart ; i++) {
-    let txtLearner = addElement(inputAr);
-    let newParag = document.createElement('p');
-    newParag.appendChild(txtLearner);
-    newParag.style.backgroundColor = random_bg_color();
-    newSection.appendChild(newParag);
-}
+//Application de la fonction :
+newTab = pickLearner(inputAr,24);
+addElement(newTab);
 
 
 
