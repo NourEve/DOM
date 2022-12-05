a5730d4d38542837c07e4cb31df69dc90ac3f0f5
Nour Everaert <nour.everaert.prof@gmail.com>
2022-12-05 15:56:54 +0100

    point 2.1. events terminé

--- Navigate & events/3.2.Events/script.js
+++ Navigate & events/3.2.Events/script.js
@@ -59,6 +59,21 @@ function addLi (color) {
   liCube.appendChild(txtCube);
 }
 
+//changer au hasard la couleur de l'arrière-plan
+function random_bg_color() {
+  var x = Math.floor(Math.random() * 255);
+  var y = Math.floor(Math.random() * 255);
+  var z = Math.floor(Math.random() * 255);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+console.log(bgColor);
+document.body.style.background = bgColor;
+}
+document.body.onkeyup = function(e) {
+  if(e.keyCode == 32) {
+    random_bg_color();
+  }
+}
+
 
 
 
