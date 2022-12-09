2e84b9ef77f5c178fc0af95f8fbb6c1668fea87c
Nour Everaert <nour.everaert.prof@gmail.com>
2022-12-09 14:16:58 +0100

    ex2

--- Forms/script.js
+++ Forms/script.js
@@ -7,3 +7,20 @@ function myFunction(){
     let x = firstname.value;
     document.getElementById("display-firstname").innerHTML = x;
 }
+
+//faire apparaitre le texte si la personne rentre 18 ou plus
+let age = document.getElementById("age");
+let truth = document.getElementById("a-hard-truth");
+age.addEventListener("keyup", hiddenTruth);
+function hiddenTruth () {
+    let x = age.value;
+    if (x >= 18) {
+        truth.style.visibility = 'visible';
+    }
+    else {
+        truth.style.visibility = 'hidden';
+    }
+}
+
+//mettre le champ en rouge si moins de 6 caractères
+
